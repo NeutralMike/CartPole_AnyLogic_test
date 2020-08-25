20d13fba8bc88a4411ffb2da4347524c2571cef3
neutralmike <neutralmike@yandex.ru>
2020-08-26 00:19:50 +0300

    gym cartpole

--- src/App.java
+++ src/App.java
@@ -1,10 +1,27 @@
-
+import Gym.CartPole;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CartPole TestCart = new CartPole();
+		for (int j = 0; j<50; j++) {
+			TestCart.Reset();
+			for (int i = 0; i< 500; i++) {
+				int action = Policy(TestCart.getState());
+				TestCart.Step(action);
+				if(TestCart.isDone() || i == 499) {
+					System.out.print("Done on step: ");
+					System.out.println(i+1);
+					TestCart.Reset();
+					break;
+				}
+			}
+		}		
+	}
+	
+	static int Policy(double[] obs) {
+		if (obs[3] > 0) return 1;
+		return 0;
 	}
 
 }
