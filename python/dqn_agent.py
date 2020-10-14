43ac7b6545156cc882e57594c7d7d26a36de6b8f
neutralmike <neutralmike@yandex.ru>
2020-10-14 12:09:29 +0300

    2.0

--- python/dqn_agent.py
+++ python/dqn_agent.py
@@ -4,48 +4,43 @@ from tensorflow.keras import layers
 
 
 class Agent:
-    lr = 1e-3
+    states = []
+    actions = []
+    targets = []
 
-    class History:
-        states = []
-        actions = []
-        rewards = []
-        saved_history = []
+    def __init__(self, num_inputs, num_actions, num_hidden=128, gamma=1, lr=1e-2):
+        self.gamma = gamma
+        self.lr = lr
+        self.num_actions = num_actions
+        self.num_inputs = num_inputs
 
-        def write(self, state, action, predicted_rewards):
-            self.states.append(state)
-            self.actions.append(action)
-            self.rewards.append(predicted_rewards)
-
-        def save(self):
-            self.saved_history.extend(zip(self.states, self. actions, self.rewards))
-            self.states = []
-            self.actions = []
-            self.rewards = []
-
-    def __init__(self, num_inputs, num_actions, num_hidden=128):
         inputs = layers.Input(shape=(num_inputs,))
-        common = layers.Dense(num_hidden, activation="relu")(inputs)
-        action = layers.Dense(num_actions, activation="softmax")(common)
-        output = layers.Dense(1)(common)
+        hidden_1 = layers.Dense(num_hidden, activation="relu")(inputs)
+        hidden_2 = layers.Dense(num_hidden)(hidden_1)
+        output = layers.Dense(num_actions, activation='linear')(hidden_2)
 
-        self.dqn = tf.keras.Model(inputs=inputs, outputs=[action, output])
+        self.dqn = tf.keras.Model(inputs=inputs, outputs=output)
         self.dqn.compile(tf.optimizers.Adam(self.lr), loss=tf.keras.losses.MeanSquaredError())
-        self.history = self.History()
 
-    def predict(self, state):
-        return self.dqn.predict(np.reshape(np.array(state), (1, -1)))[0][0]
+    def write_history(self, state, action, target):
+        self.states.append(state)
+        self.actions.append(action)
+        self.targets.append(target)
 
-    def retrain(self, gamma):
-        for i in range(len(self.history.rewards)-2, -1, -1):
-            self.history.rewards[i][self.history.actions[i]] +=  (self.history.rewards[i+1][self.history.actions[i+1]])
-        self.dqn.fit(np.array(self.history.states), np.array(self.history.rewards), epochs=1, verbose=0)
-        self.history.save()
+    def clear_history(self):
+        self.states.clear()
+        self.actions.clear()
+        self.targets.clear()
 
-    def update(self, state, action, predicted_rewards, reward, new_state, done):
-        if not done:
-            reward = reward + (np.amax(self.predict(new_state)))
-        predicted_rewards[action] = reward
-        # print(state)
-        print(predicted_rewards)
-        self.dqn.fit(np.array([state]), np.array([predicted_rewards]), epochs=1, verbose=0)
+    def predict(self, states):
+        return self.dqn.predict(np.reshape(np.array(states), (-1, self.num_inputs)))
+
+    def retrain(self):
+        #Q(s_t, a_t) = r_t + gamma * (max_a'(Q(s_t+1, a'))
+        amax_predictoins = np.amax(self.predict([self.states]), axis=1)
+        for i in range(len(self.states)-2, -1, -1):
+            self.targets[i][self.actions[i]] += self.gamma * amax_predictoins[i+1]
+
+        self.dqn.fit(np.array(self.states), np.array(self.targets), verbose=0)
+        self.clear_history()
+
